23bc62c2629d1772701f4c523374d6fcd34b6f5a
Devin Dixon <devindixon@Devins-MBP.lan>
2022-06-24 16:30:20 -0400

    Change export

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import VideoConferencing from "components/VideoConferencing"
 import Livestreaming from "components/Livestreaming"
 import Broadcasting from "components/Broadcasting"
+import Widgets from "util/Widgets"
 
-
-const returnLibrary = () => {
-    return {
-      VideoConferencing: VideoConferencing, 
-      Broadcasting : Broadcasting,
-      Livestreaming : Livestreaming,
-        // you can add here other components that you want to export
-    }
-}
-
-export default returnLibrary()
+export default {VideoConferencing, Livestreaming, Broadcasting, Widgets}
