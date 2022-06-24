b830a59c9f9037e92a9ac3abd33fe425f1cff8c4
Devin Dixon <devindixon@Devins-MBP.lan>
2022-06-24 16:47:44 -0400

    Forgot to run build

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,5 @@
-import VideoConferencing from "./VideoConferencing";
-import Livestreaming from "./Livestreaming";
-import Broadcasting from "./Broadcasting";
+import VideoConferencing from "VideoConferencing/"
+import Livestreaming from "Livestreaming/"
+import Broadcasting from "Broadcasting/"
 
-const returnLibrary = () => {
-    return {
-        VideoConferencing : VideoConferencing,
-        Livestreaming : Livestreaming,
-        Broadcasting : Broadcasting,
-        // you can add here other components that you want to export
-    }
-}
-export default returnLibrary()
+export default {VideoConferencing, Livestreaming, Broadcasting, Widgets}
